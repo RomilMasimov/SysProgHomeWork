f2ffd0e9bf67d55931ee2d2125beed9263140a54
Romil <romil.masimov@gmail.com>
2019-07-20 00:54:58 +0400

    multithreaded search

--- Finder/Source.c
+++ Finder/Source.c
@@ -3,70 +3,183 @@
 #include <tchar.h>
 #include <malloc.h>
 
+struct SearchParams
+{
+	TCHAR path[MAX_PATH];
+	TCHAR searchStr[MAX_PATH];
+	CRITICAL_SECTION criticalSection;
+};
+
+DWORD StartCLI();
+DWORD WINAPI CLI(LPVOID param);
+HANDLE* StartThreadSearch(LPCTSTR path, LPCTSTR searchStr);
+DWORD WINAPI ThreadSearch(LPVOID threadParams);
 LPTSTR ReadText(LPCTSTR path);
 BOOL WriteTextLine(LPCTSTR path, LPCTSTR str);
 BOOL Contains(LPCTSTR str1, LPCTSTR str2);
 
-struct SearchParams
+int main() {
+	//TCHAR path[MAX_PATH];
+	//_tprintf_s(_T("Enter path..> "));
+	//_tscanf_s(_T("%s"), path, _countof(path));
+	//
+	//TCHAR str[MAX_PATH];
+	//_tprintf_s(_T("Enter search str..> "));
+	//_tscanf_s(_T("%s"), str, _countof(str));
+
+	StartCLI();
+
+	//  CreateThreadpoolWork()
+	//HANDLE hThread = CreateThread(
+	 //   NULL,
+	 //   0,
+	 //   ThreadFunction,
+	 //   NULL,
+	 //   0,
+	 //   NULL
+	//);
+	//for (int i = 0; i < 20; ++i) {
+	 //   Sleep(100);
+	 //   printf("Main thread: %d\n", i);
+	//}
+	//WaitForSingleObject(hThread, INFINITE);
+	//TerminateThread(hThread, 0);
+
+
+	 //system("pause");
+	return 0;
+}
+
+DWORD StartCLI()
 {
-	LPCTSTR path;
-	LPCTSTR searchStr;
-};
+	HANDLE hThread = CreateThread
+	(
+		NULL,
+		0,
+		CLI,
+		NULL,
+		0,
+		NULL
+	);
+	WaitForSingleObject(hThread, INFINITE);
+	return 0;
+}
 
-DWORD WINAPI StartThreadSearch(LPVOID param) {
-	//int num = *((int*)param);
-	//SearchParams params = *((SearchParams*)param);
+DWORD WINAPI CLI(LPVOID param)
+{
+	DWORD num = 0;
+	HANDLE* hThreads = NULL;
+	while (TRUE)
+	{
+		if (num == 0)
+		{
+			TCHAR path[MAX_PATH];
+			_tprintf_s(_T("Enter path..> "));
+			_tscanf_s(_T("%s"), path, _countof(path));
 
-	for (INT i = 0; i < 100; ++i) {
-		Sleep(500);
-		printf("\tSecond thread: %d\n", i);
+			TCHAR str[MAX_PATH];
+			_tprintf_s(_T("Enter search str..> "));
+			_tscanf_s(_T("%s"), str, _countof(path));
+
+			hThreads = StartThreadSearch(path, str);
+
+			_tprintf_s(_T("Search started\n"));
+			num = 1;
+		}
+		else if (num == 1)
+		{
+			for (DWORD i = 0; i < _msize(hThreads) / sizeof(HANDLE); i++)
+			{
+				BOOL code = GetExitCodeThread(&hThreads[i], NULL);
+				if (code != NULL)
+				{
+					_tprintf_s(_T("OK\n"));
+				}
+			}
+		}
 	}
 	return 0;
 }
 
-//// __cdecl __stdcall
-//unsigned long __stdcall ThreadFunction1(void* param) {
-//    for (INT i = 0; i < 100; ++i) {
-//        Sleep(500);
-//        printf("\tSecond thread: %d\n", i);
-//    }
-//    return 0;
-//}
+HANDLE* StartThreadSearch(LPCTSTR path, LPCTSTR searchStr)
+{
+	WIN32_FIND_DATA fd;
+	TCHAR dirPath[MAX_PATH];
+	_tcscpy_s(dirPath, MAX_PATH, path);
+	_tcscat_s(dirPath, MAX_PATH, _T("*"));
+	HANDLE hFind = FindFirstFile(dirPath, &fd);
 
-int main() {
-	TCHAR path[MAX_PATH];
-	_tprintf_s(_T("Enter path..> "));
-	_tscanf_s(_T("%s"), path, _countof(path));
-	
-	TCHAR str[MAX_PATH];
-	_tprintf_s(_T("Enter search str..> "));
-	_tscanf_s(_T("%s"), str, _countof(path));
+	DWORD dirCount = DirectoryCount(path);
+	if (dirCount == 0)
+	{
+		LPTSTR nextPath = (LPTSTR)calloc(MAX_PATH, sizeof(TCHAR));
+		_tcscpy_s(nextPath, MAX_PATH, path);
+		_tcscat_s(nextPath, MAX_PATH, fd.cFileName);
+		_tcscat_s(nextPath, MAX_PATH, _T("\\"));
 
-	Search(path, str);
-	//WriteText(path, str);
+		struct SearchParams* params = malloc(sizeof(struct SearchParams));
+		_tcscpy_s(params->path, MAX_PATH, nextPath);
+		_tcscpy_s(params->searchStr, MAX_PATH, searchStr);
+		InitializeCriticalSection(&(params->criticalSection));
 
-   //  CreateThreadpoolWork()
-   /*HANDLE hThread = CreateThread(
-	   NULL,
-	   0,
-	   ThreadFunction,
-	   NULL,
-	   0,
-	   NULL
-   );
-   for (int i = 0; i < 20; ++i) {
-	   Sleep(100);
-	   printf("Main thread: %d\n", i);
-   }
-   WaitForSingleObject(hThread, INFINITE);
+		HANDLE hThread = CreateThread
+		(
+			NULL,
+			0,
+			ThreadSearch,
+			params,
+			0,
+			NULL
+		);
+		return hThread;
+	}
 
-   TerminateThread(hThread, 0);*/
+	HANDLE* hThreads = (HANDLE*)calloc(dirCount, sizeof(HANDLE));
+	do
+	{
+		DWORD count = 0;
 
-	return 0;
+		if (hFind == INVALID_HANDLE_VALUE ||
+			_tcscmp(_T(".."), fd.cFileName) == 0 ||
+			_tcscmp(_T("."), fd.cFileName) == 0)
+		{
+			continue;
+		}
+		if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		{
+			LPTSTR nextPath = (LPTSTR)calloc(MAX_PATH, sizeof(TCHAR));
+			_tcscpy_s(nextPath, MAX_PATH, path);
+			_tcscat_s(nextPath, MAX_PATH, fd.cFileName);
+			_tcscat_s(nextPath, MAX_PATH, _T("\\"));
+
+			struct SearchParams* params = malloc(sizeof(struct SearchParams));
+			_tcscpy_s(params->path, MAX_PATH, nextPath);
+			_tcscpy_s(params->searchStr, MAX_PATH, searchStr);
+			InitializeCriticalSection(&(params->criticalSection));
+
+			hThreads[count] = CreateThread
+			(
+				NULL,
+				0,
+				ThreadSearch,
+				params,
+				0,
+				NULL
+			);
+			++count;
+		}
+
+	} while (FindNextFile(hFind, &fd));
+	FindClose(hFind);
+	return hThreads;
 }
 
-VOID Search(LPCTSTR path, LPCTSTR searchStr/*, INT offset*/)
+DWORD WINAPI ThreadSearch(LPVOID param)
 {
+	struct SearchParams params = *((struct SearchParams*)param);
+	LPCTSTR path = params.path;
+	LPCTSTR searchStr = params.searchStr;
+
 	WIN32_FIND_DATA fd;
 	//LPTSTR filePath = (LPTSTR)calloc(MAX_PATH, sizeof(TCHAR));
 	TCHAR filePath[MAX_PATH];
@@ -74,40 +187,29 @@ VOID Search(LPCTSTR path, LPCTSTR searchStr/*, INT offset*/)
 	_tcscat_s(filePath, MAX_PATH, _T("*"));
 	HANDLE hFind = FindFirstFile(filePath, &fd);
 
-	do 
+	do
 	{
-		//Sleep(50);
 		if (hFind == INVALID_HANDLE_VALUE ||
 			_tcscmp(_T(".."), fd.cFileName) == 0 ||
-			_tcscmp(_T("."), fd.cFileName) == 0) 
+			_tcscmp(_T("."), fd.cFileName) == 0)
 		{
 			continue;
 		}
-	//if (hFind != INVALID_HANDLE_VALUE)
-		//{
 		LPTSTR nextPath = (LPTSTR)calloc(MAX_PATH, sizeof(TCHAR));
 		_tcscpy_s(nextPath, MAX_PATH, path);
 		_tcscat_s(nextPath, MAX_PATH, fd.cFileName);
-		if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) 
+		if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 		{
-			//for (INT i = 0; i < offset * 2; ++i) {
-			//	printf(" ");
-			//}	
 			_tcscat_s(nextPath, MAX_PATH, _T("\\"));
-			Search(nextPath, searchStr/*, offset + 1*/);
+			struct SearchParams* nextParams = malloc(sizeof(struct SearchParams));
+			_tcscpy_s(nextParams->path, MAX_PATH, nextPath);
+			_tcscpy_s(nextParams->searchStr, MAX_PATH, searchStr);
+			nextParams->criticalSection = params.criticalSection;
+			ThreadSearch(&nextParams);
+			free(nextParams);
 		}
 		else
 		{
-			//for (int i = 0; i < offset * 2; ++i) {
-			//	printf(" ");
-			//}
-			//ULARGE_INTEGER ul;
-			//ul.HighPart = fd.nFileSizeHigh;
-			//ul.LowPart = fd.nFileSizeLow;
-			//ULONGLONG fileSize = ul.QuadPart;
-			//SetFilePointer(hFind, 0, 0, FILE_BEGIN);
-			//_tprintf(_T("%*s%ul\n"), -70 + offset * 2, fd.cFileName, fileSize);
-
 			LPTSTR fileText = ReadText(nextPath);
 			if (fileText != NULL)
 			{
@@ -115,15 +217,47 @@ VOID Search(LPCTSTR path, LPCTSTR searchStr/*, INT offset*/)
 				free(fileText);
 				if (cResult)
 				{
-					WriteTextLine("C:\\Users\\Romil\\Desktop\\result.txt", nextPath);
+					EnterCriticalSection(&(params.criticalSection));
+					WriteTextLine("C:\\Temp\\result.txt", nextPath);
+					LeaveCriticalSection(&(params.criticalSection));
 				}
 			}
 
 		}
-		_tcscpy_s(filePath, MAX_PATH, path);
+		//_tcscpy_s(filePath, MAX_PATH, path);
 	} while (FindNextFile(hFind, &fd));
 	FindClose(hFind);
-	//}
+
+	return 0;
+}
+
+DWORD DirectoryCount(LPCTSTR path)
+{
+	DWORD result = 0;
+
+	WIN32_FIND_DATA fd;
+	TCHAR dirPath[MAX_PATH];
+	_tcscpy_s(dirPath, MAX_PATH, path);
+	_tcscat_s(dirPath, MAX_PATH, _T("*"));
+	HANDLE hFind = FindFirstFile(dirPath, &fd);
+
+	do
+	{
+		if (hFind == INVALID_HANDLE_VALUE ||
+			_tcscmp(_T(".."), fd.cFileName) == 0 ||
+			_tcscmp(_T("."), fd.cFileName) == 0)
+		{
+			continue;
+		}
+		if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		{
+			++result;
+		}
+
+	} while (FindNextFile(hFind, &fd));
+	FindClose(hFind);
+
+	return result;
 }
 
 LPTSTR ReadText(LPCTSTR path)
@@ -166,15 +300,13 @@ BOOL WriteTextLine(LPCTSTR path, LPCTSTR str)
 
 	if (hFile != INVALID_HANDLE_VALUE)
 	{
-		LPTSTR writeStr = (LPTSTR)calloc(lstrlen(str) + 1, sizeof(TCHAR));
-		DWORD strSize = (lstrlen(str) + 1) * (sizeof(TCHAR));
-		_tcscat_s(writeStr, strSize, str);
-		*(writeStr + lstrlen(writeStr)) = _T('\n');
+		DWORD strSize = lstrlen(str) * sizeof(TCHAR);
+		TCHAR nextLne = _T('\n');
 
 		SetFilePointer(hFile, 0, NULL, FILE_END);
-		BOOL wfResult = WriteFile(hFile, writeStr, strSize, NULL, NULL);
+		BOOL wfResult = WriteFile(hFile, str, strSize, NULL, NULL);
+		wfResult = WriteFile(hFile, &nextLne, sizeof(TCHAR), NULL, NULL);
 		CloseHandle(hFile);
-		free(writeStr);
 
 		return wfResult;
 	}
